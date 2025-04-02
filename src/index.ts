9206fc76878787ea0b700f07338bd5eaa6131b4b
Jayneel Mahival <jm.mahival@gmail.com>
2025-04-03 02:54:57 +0530

    done  signin backend point

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,18 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import UserModel from './Schemas/userschema';
-
 //! this config is used to get the port and mongoUri from the environment variables and validates them.
 const config = {
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGO_URI || (() => {
     throw new Error('MONGO_URI is not defined in environment variables');
-  })()
+   })(),
+   JWT_PASSWORD: process.env.JWT_PASSWORD || (() => {
+    throw new Error('JWT_PASSWORD is not defined in environment variables');
+   })()
 } as const;
 
 const app = express();
@@ -29,7 +32,35 @@ app.post('/api/v1/signup', async (req, res) => {
    }
  });
 
-app.post('/api/v1/signin', (req, res) => { });
+app.post("/api/v1/signin", async (req, res) => {
+   const username = req.body.username;
+   const password = req.body.password;
+   try {
+      const existingUser = await UserModel.findOne({
+         username,
+         password
+      })
+      if (existingUser) {
+         const token = jwt.sign({
+            id: existingUser._id
+         }, config.JWT_PASSWORD)
+
+         res.json({
+            token
+         })
+      } else {
+         res.status(403).json({
+            message: "Incorrect credentials"
+         })
+      }
+   } catch (error) {
+      res.status(500).json({
+         message: "Internal server error",
+         error
+      })
+   }
+   
+})
 
 
 //*ccontent routes
