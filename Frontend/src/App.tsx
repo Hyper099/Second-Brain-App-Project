46ab6f59618ff8c04fa0c2234ce857677da2eae3
Jayneel Mahival <jm.mahival@gmail.com>
2025-04-24 04:54:58 +0530

    vercel anal

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ProtectedRoute from "./context/ProtectedRoute";
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
+          <Analytics/>
           <Route path="/" element={<AuthPages />} />
           <Route path="/login" element={<AuthPages/>}/>
           <Route path="/dashboard" element={
